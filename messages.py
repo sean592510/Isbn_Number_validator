9ca6c64c9f9db667fd920ee12a10d588efe67243
John <cj592510@gmail.com>
2025-11-28 19:45:07 +0530

    Add messages for ISBN validation

---
+++ messages.py
@@ -0,0 +1,9 @@
+# messages.py
+ENTER_COMMA = "Enter comma-separated values."
+LENGTH_NOT_NUMBER = "Length must be a number."
+INVALID_LENGTH = "Length should be 10 or 13."
+INVALID_CHAR = "Invalid character was found."
+WRONG_LENGTH = "ISBN-{length} code should be {length} digits long."
+VALID = "Valid ISBN Code."
+INVALID = "Invalid ISBN Code."
+PROMPT = "Please provide ISBN and length separated by a comma: "
